9bc1690eced989d9adb31cee5806b46cd0f21e8e
amofen <cm_aouffen@esi.dz>
2017-04-25 16:23:19 +0100

    Lots get service

---
+++ Sentinel-Mobile/Presentation/Controlers/InitController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+using Sentinel_Mobile.Data.Synchronisation;
+using Sentinel_Mobile.Model.DTO;
+using Sentinel_Mobile.Model.Domain.Vehicules;
+using Sentinel_Mobile.Model.Util;
+using Sentinel_Mobile.Presentation.Util;
+using Sentinel_Mobile.Business;
+
+namespace Sentinel_Mobile.Presentation.Controlers
+{
+    class InitController
+    {
+        public void initLots()
+        {
+            
+            LotManager lotManager = new LotManager();
+            List<Lot> listLot = lotManager.getLotsPrevu(null);
+            try
+            {
+                lotManager.saveLots(listLot);
+            }
+            catch (Exception e)
+            {
+                MessagingService.showErrorMessage(e.Message);   
+            }
+         
+        }
+
+        public int getNombreVehiculesArrivage()
+        {
+            LotManager lotManager = new LotManager();
+            List<Lot> lots = lotManager.getCacheLots();
+            VehiculeManager vehiculeManager = new VehiculeManager();
+            int cumulVehicules = 0;
+            foreach (Lot lot in lots)
+            {
+                cumulVehicules += vehiculeManager.getNombreVehiculeLot(lot.Id);
+            }
+            return cumulVehicules;
+        }
+    }
+}
